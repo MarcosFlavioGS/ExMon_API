71df44915a397999de45d43bf6082941a6dab6c9
Marcos Flavio <mfghost69@gmail.com>
2023-04-14 21:31:21 -0300

    Adding a new controller: trainers_controller

---
+++ lib/ex_mon_web/controllers/trainers_controller.ex
@@ -0,0 +1,15 @@
+defmodule ExMonWeb.TrainersController do
+  use ExMonWeb, :controller
+
+  def create(conn, params) do
+    params
+    |> ExMon.create_trainer() # Call facade
+    |> handle_response(conn)
+  end
+
+  def handle_response({:ok, trainer}, conn) do
+    conn
+    |> put_status(:ok) # Writing ok is the same as writing 200(search for Phoenix:http status)
+    |> render("create.json", trainer: trainer)
+  end
+end
